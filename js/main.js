26c472eaba7dd39eb8eaf9b6341f2372caf04afd
pskalkii <pawelskalski97@gmail.com>
2022-05-04 20:45:26 +0200

    dry zastosowane

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let button= document.querySelector('button')
-document.querySelector('button').onclick=function(){
-    alert('Klik działa!')
+button.onclick=function(){
+    alert('Klik działa!');
 }
     
 
